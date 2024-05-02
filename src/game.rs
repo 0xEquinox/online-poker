26ad7607dd4fd0691eccd4a6dcfb63e0ed907ecd
0xEquinox <williamjmcdonald@proton.me>
2024-05-02 08:39:49 -0700

    made some changes

--- src/game.rs
+++ src/game.rs
@@ -9,6 +9,16 @@ use ws::WebSocket;
 
 #[derive(Serialize, Clone)]
 #[serde(crate = "rocket::serde")]
+pub enum GameState {
+    Deal,
+    Preflop,
+    Flop,
+    Turn,
+    River,
+}
+
+#[derive(Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Game {
     pub settings: GameSettings,
     pub players: Vec<Player>,
@@ -18,6 +28,8 @@ pub struct Game {
     pub small_blind_pos: i32,
     pub dealer_pos: i32,
     pub deck: Deck,
+    pub current_players_turn: u32,
+    pub state: GameState,
 }
 
 impl Game {
@@ -31,6 +43,8 @@ impl Game {
             small_blind_pos: 0,
             dealer_pos: 0,
             deck: Deck::new(),
+            current_players_turn: 0,
+            state: GameState::Deal,
         }
     }
 }
@@ -58,10 +72,17 @@ impl GameSettings {
 #[derive(Serialize, Deserialize, Copy, Clone)]
 #[serde(crate = "rocket::serde")]
 pub enum Action {
+    Start,
     Fold,
     Check,
     Call,
+    YourTurn,
+    Anti(u64),
     Raise(u64),
+    DealPlayer([Card; 2]),
+    DealFlop([Card; 3]),
+    DealTurn([Card; 1]),
+    DealRiver([Card; 1]),
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone)]
@@ -75,13 +96,49 @@ pub struct Message {
 #[post("/make_move", data = "<message>", format = "application/json")]
 pub fn make_move(message: Json<Message>, lobbies: &State<Lobbies>, queue: &State<Sender<Message>>) {
     match message.action {
-        Action::Fold => fold(message.player_id, message.room_code, lobbies),
-        Action::Check => check(),
-        Action::Call => call(message.player_id, message.room_code, lobbies),
-        Action::Raise(amount) => raise(amount, message.player_id, message.room_code, lobbies),
+        Action::Fold => {
+            fold(message.player_id, message.room_code, lobbies);
+            next_turn(message.room_code, lobbies, queue);
+        }
+        Action::Check => {
+            check();
+            next_turn(message.room_code, lobbies, queue);
+        }
+        Action::Call => {
+            call(message.player_id, message.room_code, lobbies);
+            next_turn(message.room_code, lobbies, queue);
+        }
+        Action::Raise(amount) => {
+            raise(amount, message.player_id, message.room_code, lobbies);
+            next_turn(message.room_code, lobbies, queue);
+        }
+        Action::Start => start(message.room_code, lobbies, queue),
+        _ => eprintln!("Invalid Move"),
     }
 
     let _ = queue.send(*message);
+}
+
+fn next_turn(room_code: i32, lobbies: &State<Lobbies>, queue: &State<Sender<Message>>) {
+    println!("Moving to next player");
+    let game = &lobbies.lobbies.get(&room_code).unwrap().game;
+
+    let current_player_id = game.current_players_turn;
+    let mut next_player_id: i32 = current_player_id as i32 + 1;
+
+    // Check if it has to be wrapped around
+    if current_player_id as usize == game.players.len() - 1 {
+        next_player_id = 0;
+    }
+
+    // Let the next player know it is their turnYourTurn
+    let next_player_message = Message {
+        action: Action::YourTurn,
+        room_code,
+        player_id: next_player_id,
+    };
+
+    let _ = queue.send(next_player_message);
 }
 
 fn check() {
@@ -149,4 +206,41 @@ fn raise(amount: u64, player_id: i32, room_code: i32, lobbies: &State<Lobbies>) 
     player.current_bet += amount;
     game.pot += amount;
     game.current_bet = player.current_bet;
+}
+
+fn start(room_code: i32, lobbies: &State<Lobbies>, queue: &State<Sender<Message>>) {
+    let mut binding = lobbies.lobbies.get_mut(&room_code).unwrap();
+    let lobby = binding.value_mut();
+    let game = &mut lobby.game;
+    let players = &mut game.players;
+
+    // Construct a message for the event queue, will need one message per player
+    for player in &mut *players {
+        let message = Message {
+            action: Action::DealPlayer([game.deck.draw(), game.deck.draw()]),
+            player_id: player.id, // -1 for dealer
+            room_code,
+        };
+
+        let _ = queue.send(message);
+    }
+
+    // Also send out the anti message
+    for player in players {
+        let message = Message {
+            action: Action::Anti(game.settings.small_blind as u64),
+            player_id: player.id, // -1 for dealer
+            room_code,
+        };
+
+        let _ = queue.send(message);
+    }
+    // Tell the first player it's their turn
+    let message = Message {
+        action: Action::YourTurn,
+        player_id: 0,
+        room_code,
+    };
+
+    let _ = queue.send(message);
 }
--- static/script.js
+++ static/script.js
@@ -1,17 +1,49 @@
+let roomCode = 0;
+let pot = 0;
+let player = { // Player state
+    id: null,
+    money: 0,
+    hand: [],
+    currentBet: 0,
+    roomCode: 0,
+    isMyTurn: false
+}; // TODO: make not global variables
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('join-lobby-form');
-    const player = { // Player state
-        id: null,
-        money: 0,
-        hand: [],
-        currentBet: 0,
-        roomCode: 0
-    };
+    // Update buttons based on player's current bet
+    const raiseButton = document.getElementById('raise');
+
+    // Add event listeners to buttons
+    document.getElementById('check').addEventListener('click', function() {
+        makeMove('Check');
+    });
+
+    document.getElementById('fold').addEventListener('click', function() {
+        makeMove('Fold');
+    });
+
+    document.getElementById('call').addEventListener('click', function() {
+        makeMove('Call');
+    });
+
+    document.getElementById('start').addEventListener('click', function() {
+        makeMove('Start');
+    });
+
+    document.getElementById('raise').addEventListener('click', () => {
+        const raiseAmount = parseInt(document.getElementById('raise-amount').value);
+        makeMove('Raise', raiseAmount);
+    });
+    // Update raise amount display when slider value changes
+    document.getElementById('raise-amount').addEventListener('input', function() {
+        document.getElementById('raise-value').innerText = this.value;
+    });
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        const roomCode = document.getElementById('room-code').value;
+        roomCode = document.getElementById('room-code').value;
 
         player.roomCode = parseInt(roomCode);
 
@@ -35,6 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('game-container').style.display = 'block';
 
             player.id = lobby.game.players[lobby.game.players.length - 1].id
+            player.money = lobby.game.settings.starting_money;
 
             updateUI()
         })
@@ -46,51 +79,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Function to update UI based on player state
     function updateUI() {
-        // Update pot display
-        document.getElementById('pot').innerText = `Pot: $0`; // Update with actual pot amount
+        // Update information
+        document.getElementById('pot').innerText = `Pot: $${pot}`; // Update with actual pot amount
+        document.getElementById('money').innerText = `Money: $${player.money}`;
+        document.getElementById('current-bet').innerText = `Bet: $${player.currentBet}`;
+        document.getElementById('is-my-turn').innerText = `Is My Turn: ${player.isMyTurn}`;
 
         // Update player's cards display
         const playerCards = document.getElementById('player-cards');
-        playerCards.innerHTML = ''; // Clear previous cards
-        player.hand.forEach(card => {
+        playerCards.innerHTML = ""; // Clear previous cards in new round
+        playerHandToString(player.hand).forEach(card => {
             const cardDiv = document.createElement('div');
             cardDiv.classList.add('card');
-            cardDiv.textContent = card; // Assuming card is a string representation
+            cardDiv.textContent = card;
             playerCards.appendChild(cardDiv);
         });
 
-        // Update buttons based on player's current bet
-        const raiseButton = document.getElementById('raise');
-        raiseButton.innerText = `Raise (${player.currentBet * 2})`; // Example: Update raise button text with double the current bet amount
-
-        // Add event listeners to buttons
-        document.getElementById('check').addEventListener('click', function() {
-            makeMove('Check');
-        });
-
-        document.getElementById('fold').addEventListener('click', function() {
-            makeMove('Fold');
-        });
-
-        document.getElementById('call').addEventListener('click', function() {
-            makeMove('Call');
-        });
-
-        document.getElementById('raise').addEventListener('click', () => {
-            const raiseAmount = parseInt(document.getElementById('raise-amount').value);
-            makeMove('Raise', raiseAmount);
-        });
-        // Update raise amount display when slider value changes
-        document.getElementById('raise-amount').addEventListener('input', function() {
-            document.getElementById('raise-value').innerText = this.value;
-        });
     }
 
     // Function to send move to the server
     function makeMove(action, amount = null) {
+        if ((!player.isMyTurn) && player.id !== 0 && player.money === 0 && player.hand === []) return;
+
         const message = {
             action: action,
-            room_code: player.roomCode, // Assuming player has roomCode property
+            room_code: player.roomCode,
             player_id: player.id
         };
 
@@ -98,7 +111,6 @@ document.addEventListener('DOMContentLoaded', function() {
             message.action = { "Raise": amount };
         }
 
-        // Make a POST request to the server to make a move
         fetch('/api/make_move', {
             method: 'POST',
             headers: {
@@ -108,58 +120,102 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => {
             console.error('Error making move:', error);
-            // Handle errors, e.g., display an error message to the user
         });
+
+        player.isMyTurn = false;
     }
+
+    function subscribe(uri) {
+        var retryTime = 1;
+
+        function connect(uri) {
+            const events = new EventSource(uri);
+
+            events.addEventListener("message", (ev) => {
+            console.log("raw data", JSON.stringify(ev.data));
+            console.log("decoded data", JSON.stringify(JSON.parse(ev.data)));
+            const msg = JSON.parse(ev.data);
+
+            // Handle the messages that come from the stream here
+            handleEvent(msg);
+            });
+
+            events.addEventListener("open", () => {
+            setConnectedStatus(true);
+            console.log(`connected to event stream at ${uri}`);
+            retryTime = 1;
+            });
+
+            events.addEventListener("error", () => {
+            setConnectedStatus(false);
+            events.close();
+
+            let timeout = retryTime;
+            retryTime = Math.min(64, retryTime * 2);
+            console.log(`connection lost. attempting to reconnect in ${timeout}s`);
+            setTimeout(() => connect(uri), (() => timeout * 1000)());
+            });
+        }
+
+        connect(uri);
+        }
+
+        function setConnectedStatus(status) {
+
+        }
+
+        function updatePot(potAmount) {
+            // Update the pot display with the new pot amount
+            document.getElementById('pot').innerText = `Pot: $${potAmount}`;
+        }
+
+        function handleEvent(eventData) {
+            console.log("Received event:", eventData);
+
+            console.log("This message is for player: ", eventData.player_id, " and room: ", eventData.room_code);
+            if (eventData.room_code !== roomCode) {
+                //return;
+            }
+
+            if (eventData.action.Raise !== undefined) {
+                raiseAmount = eventData.action.Raise;
+                if (player.id == eventData.player_id) {
+                    player.money -= raiseAmount;
+                    player.currentBet += raiseAmount;
+                }
+                pot += raiseAmount;
+            } else if (player.id === eventData.player_id && eventData.action.DealPlayer !== undefined) {
+                player.hand = eventData.action.DealPlayer;
+                console.log("Player Hand: ", player.hand);
+            } else if (eventData.action.Anti !== undefined) {
+                anti = eventData.action.Anti;
+                if (player.id == eventData.player_id) {
+                    player.money -= anti;
+                    player.currentBet += anti;
+                }
+                pot += anti;
+            } else if (eventData.action === "YourTurn" && player.id === eventData.player_id) {
+                console.log("it is now this players turn");
+                player.isMyTurn = true;
+            }
+
+            updateUI();
+        }
+
+    function playerHandToString(hand) {
+        stringHand = [];
+        hand.forEach(card =>  {
+            if (card.value === 1) stringHand.push("A " + card.suit);
+            else if (card.value === 11) stringHand.push("J " + card.suit);
+            else if (card.value === 12) stringHand.push("Q " + card.suit);
+            else if (card.value === 13) stringHand.push("K " + card.suit);
+            else stringHand.push(card.value + " " + card.suit);
+        });
+
+        return stringHand;
+    }
+
+        subscribe("/api/events");
    });
 
 // Connects to the event stream for real time updates
-function subscribe(uri) {
-  var retryTime = 1;
-
-  function connect(uri) {
-    const events = new EventSource(uri);
-
-    events.addEventListener("message", (ev) => {
-      console.log("raw data", JSON.stringify(ev.data));
-      console.log("decoded data", JSON.stringify(JSON.parse(ev.data)));
-      const msg = JSON.parse(ev.data);
-
-      // Handle the messages that come from the stream here
-      handleEvent(msg);
-    });
-
-    events.addEventListener("open", () => {
-      setConnectedStatus(true);
-      console.log(`connected to event stream at ${uri}`);
-      retryTime = 1;
-    });
-
-    events.addEventListener("error", () => {
-      setConnectedStatus(false);
-      events.close();
-
-      let timeout = retryTime;
-      retryTime = Math.min(64, retryTime * 2);
-      console.log(`connection lost. attempting to reconnect in ${timeout}s`);
-      setTimeout(() => connect(uri), (() => timeout * 1000)());
-    });
-  }
-
-  connect(uri);
-}
-
-function updatePot(potAmount) {
-    // Update the pot display with the new pot amount
-    document.getElementById('pot').innerText = `Pot: $${potAmount}`;
-}
-
-function handleEvent(eventData) {
-    console.log("Received event:", eventData);
-
-    if (eventData.action.Raise !== null) {
-        updatePot(eventData.action.Raise);
-    }
-}
-
-subscribe("/api/events")
